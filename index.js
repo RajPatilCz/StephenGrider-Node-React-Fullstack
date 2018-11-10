a5e1376c11b5dbdef6473668e31b3aece6d992b5
Rigo De La Torre <rigoddd@gmail.com>
2018-11-09 17:59:43 -0800

    project setup, architecture

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
+require('./services/passport')
+
+
 const app = express();
 
+require('./routes/authRoutes')(app)
 
-//route handler
-app.get('/', (req,res)=>{
-  res.send({bye:'buddy'})
-})
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT)
