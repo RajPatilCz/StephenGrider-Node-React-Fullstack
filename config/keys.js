1c22c3250795791795082dc3dba2e289a75bcadd
Rigo De La Torre <rigoddd@gmail.com>
2018-11-14 14:37:32 -0800

    addedinitial react side

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
 module.exports = {
-  googleClientID:'583300311607-5vql1jiqkbe6h5qlf7olm4q611jle6m9.apps.googleusercontent.com',
-  googleClientSecret:'-ynpGgJ0_bbuujpKXMfSKnrC',
-  mongoURI: 'mongodb://rigo:password1@ds159273.mlab.com:59273/emaily-dev1',
-  cookieKey:'jweofij209ru209fsdfofjpoj2r09jsofjspfjpojkwefrwe'
-}
+  googleClientID:
+    "583300311607-5vql1jiqkbe6h5qlf7olm4q611jle6m9.apps.googleusercontent.com",
+  googleClientSecret: "-ynpGgJ0_bbuujpKXMfSKnrC",
+  mongoURI: "mongodb://rigo:password1@ds159273.mlab.com:59273/emaily-dev1",
+  cookieKey: "jweofij209ru209fsdfofjpoj2r09jsofjspfjpojkwefrwe"
+};
