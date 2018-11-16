178e2cb86dfc8e0fd9145b807e5dc532e251899a
Rigo De La Torre <rigoddd@gmail.com>
2018-11-15 19:31:35 -0800

    fixed env varibles and added o readme

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,9 @@
-module.exports = {
-  googleClientID:"583300311607-5vql1jiqkbe6h5qlf7olm4q611jle6m9.apps.googleusercontent.com",
-  googleClientSecret: "-ynpGgJ0_bbuujpKXMfSKnrC",
-  mongoURI: "mongodb://rigo:password1@ds159273.mlab.com:59273/emaily-dev1",
-  cookieKey: "jweofij209ru209fsdfofjpoj2r09jsofjspfjpojkwefrwe"
-};
+
+// keys.js - figure out what set of credentials to return
+if (process.env.NODE_ENV === 'production') {
+    console.log('production ran ', process.env.NODE_ENV)
+   module.exports = require('./prod');
+} else {
+  console.log('development ran ', process.env.NODE_ENV)
+   module.exports = require('./dev');
+}
